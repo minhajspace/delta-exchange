4e53006cbddbab5dba424aeeda631cfffe7a472b
Minhaj Ahmad Khan <51471123+minhajspace@users.noreply.github.com>
2022-02-16 16:25:14 +0530

    bind table data

--- src/component/Cryptotable.js
+++ src/component/Cryptotable.js
@@ -1,17 +1,116 @@
 import React,{useEffect} from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch,useSelector } from 'react-redux'
 import {getCryptoDetails} from '../redux/actions/crypto.action'
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
 
 
+const columns = [
+  { id: 'symbol', label: 'Symbol', minWidth: 170 },
+  { id: 'description', label: 'Description', minWidth: 100 },
+  {
+    id: 'underlying_asset',
+    label: 'Underlying Asset',
+    minWidth: 170,
+    align: 'right',
+    format: (value) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'Mark Price',
+    label: 'Mark Price',
+    minWidth: 170,
+    align: 'right',
+    format: (value) => value.toLocaleString('en-US'),
+  },
+  
+];
+
+function createData(name, code, population, size) {
+  const density = population / size;
+  return { name, code, population, size, density };
+}
 
 export default function  CryptoTable () {
   const dispatch = useDispatch()
+  const coins = useSelector(state => state.crypto.coins)
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+
+
+
+
+
+
+console.log("coins==",coins)
 
   useEffect(()=>{
      dispatch(getCryptoDetails())
   },[])
   return <>
-       <div>hello</div> 
+       <Paper sx={{ width: '100%' }}>
+      <TableContainer sx={{ maxHeight: "100%" }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {coins
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {columns.map((column) => {
+                      const value =column.label ==="Underlying Asset" ? row.underlying_asset.symbol :  row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === 'number'
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={coins.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
           
   </>
 }
